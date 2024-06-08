b18e88de040aa737e723b919b88881894ece2bb4
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-08 12:42:00 +0530

    update

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import Youtube from './youtube'
 
 function App() {
+  const username = "Zaid";
   return (
     <>
         <h1>Vite ++ React</h1>
+        <h1>Muhammad {username}</h1>
         <Youtube/>   
     </>
   )
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const ReactElement =  {     React does't know what are types like type, props, children. 
+//     type: 'a',
+//     props: {
+//         href: "https://google.com",
+//         target: "_blank"
+//     },
+//     children: "Click me to visit Google"
+// }
+
+//Right Syntax to write react Element
+const AreactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: "_blank"},
+    'click to Visit gooGle'
+)
+
+
+function MyApp() {
+    return(
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (<a href="https://google.com" target="_blank">Visit Google </a>
+
+)
+    
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+   <App/>
 )
