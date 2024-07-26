6e471fffa64d9dd6c91024ab4734fbd9d5fa8a3e
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-26 23:36:17 +0530

    add code

--- deltafirst/src/Product.jsx
+++ deltafirst/src/Product.jsx
@@ -1,10 +1,11 @@
 import './Product.css'
 
-function Product({title, price}){
+function Product({title, price , description = "this is a Good product"}){
     return(
         <div className="Product">
         <h3>{title}</h3>
         <h5>Price: {price}</h5>
+        <p>description: {description}</p>
         </div>
     )
 }
--- deltafirst/src/ProductTab.jsx
+++ deltafirst/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product";
 function ProductTab(){
     return(
         <>
-        <Product title="Phone" price="30K"/>
+        <Product title="Phone" price="30K" description="This is Phone"/>
         <Product title="Laptop" price="40000"/>
-        <Product title="Smart Watch" price={400 + 100}/>
+        <Product title="Smart Watch" price={400 + 100} description="This is SmartWatch"/>
         </>
     )
 }
