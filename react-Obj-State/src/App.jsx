b24a4a73b05f35e01c350e94b3a3e11df4e8b7a4
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-08-07 00:12:57 +0530

    add code

--- react-Obj-State/src/App.jsx
+++ react-Obj-State/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { sum } from './helper.js'
 import Lottery from './Lottery.jsx'
 
 function App() {
+
+  let winCondition = (ticket) => {
+    // return sum(ticket) === 15
+    return ticket[0] === 0;
+  }
+
   return (
     <>
-      <Lottery n={3} winningSum={15}/>
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
--- react-Obj-State/src/Lottery.jsx
+++ react-Obj-State/src/Lottery.jsx
@@ -3,9 +3,9 @@ import './Lottery.css';
 import { genTicket, sum } from "./helper";
 import Ticket from "./Ticket";
 
-export default function Lottery({n, winningSum}) {
+export default function Lottery({n, winCondition}) {
     let [ticket, setTicket] = useState(genTicket(n));
-    let winingGame = sum(ticket) === winningSum
+    let winingGame = winCondition(ticket)
 
     let buyTicket = () => {
         setTicket(genTicket(n))
